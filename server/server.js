c96fdf1e0bb236777aa1e70a5ded69d8d9b51e24
Nabhahn Kamaal <NabhahnK@gmail.com>
2023-05-13 17:30:42 -0700

    added authRoutes to server.js and added periods to commentsd

--- server/server.js
+++ server/server.js
@@ -1,32 +1,35 @@
-// import dependencies
+// import dependencies.
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// create port
+// import in project dependencies.
+const authRoutes = require('./routes/authRoutes');
+
+// create port.
 const PORT = process.env.PORT || process.env.API_PORT;
 
-// create express app and set up middleware
+// create express app and set up middleware.
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// create server
+// create server.
 const server = http.createServer(app);
 
-// Connect to database
+// Connect to database.
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
-        // launch server and listen
+        // launch server and listen.
         server.listen(PORT, () => {
             console.log(`Server is listening on ${PORT}`);
         });
     })
     .catch(err => {
-        // If error, tell user and give error in console
+        // If error, tell user and give error in console.
         console.log('Database connection failed. Server not started.');
         console.log(err);
     })
