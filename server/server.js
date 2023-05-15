e59f04fefb42f6c423eba099e18cf56c7c575b2c
Nabhahn Kamaal <NabhahnK@gmail.com>
2023-05-15 13:03:45 -0700

    added routes to server.js to test

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// import in project dependencies.
+// import in-project dependencies.
 const authRoutes = require('./routes/authRoutes');
 
 // create port.
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || process.env.API_PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// register routes
+app.use('/api/auth', authRoutes);
 
 // create server.
 const server = http.createServer(app);
