52435d7384793dd7f18be4e83ae06becda032000
Nabhahn Kamaal <NabhahnK@gmail.com>
2023-05-15 14:45:45 -0700

    reoedered imports and created validaton schema

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,18 @@
 // import router from express.
 const express = require('express');
 const router = express.Router();
+// importing logic from controllers
+const authControllers = require('../controllers/auth/authControllers');
 // importing validation dependencies
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
 
-// importing logic from controllers
-const authControllers = require('../controllers/auth/authControllers');
+// create validation schema
+const registerSchema = Joi.object({
+    username: Joi.string().min(3).max(12),
+    password: Joi.string().min(6).max(12),
+    main: Joi.string.email(),
+});
 
 // route to register new user.
 router.post('/register', authControllers.controllers.postRegister);
