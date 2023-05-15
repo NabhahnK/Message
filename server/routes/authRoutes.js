debfda57789cc906b09edf059f688fa361ea41a8
Nabhahn Kamaal <NabhahnK@gmail.com>
2023-05-15 13:58:28 -0700

    imported and integrated authControllers into authRoutes to use routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+// importing logic from controllers
+const authControllers = require('../controllers/auth/authControllers');
+
 // route to register new user.
-router.post('/register', );
+router.post('/register', authControllers.controllers.postRegister);
 
 // route for user login.
-router.post('/login', );
+router.post('/login', authControllers.controllers.postLogin);
 
 module.exports = router;
