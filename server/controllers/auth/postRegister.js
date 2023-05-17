4a270d3c470883113734789b8817b088f6abb2ca
Nabhahn Kamaal <NabhahnK@gmail.com>
2023-05-16 18:36:18 -0700

    created the logic in postRegister with fake token to test logic

--- server/controllers/auth/postRegister.js
+++ server/controllers/auth/postRegister.js
@@ -1,6 +1,47 @@
+// import user model
+const User = require('../../models/user');
+// import bcrypt
+const bcrypt = require('bcryptjs');
+
 // declaring postRegister logic
 const postRegister = async (req, res) => {
-    res.send('register route');
+    try {
+        // get user entered info from req.body
+        const { username, mail, password } = req.body;
+
+        // check if user exists
+        const userExists = await User.exists({ mail: mail.toLowerCase() });
+
+        if (userExists) {
+            return res.status(409).send('E-mail already in use.');
+        }
+
+        // encrypt password
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        // create user and save in DB
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPassword,
+        });
+
+        // create jwt token
+        const token = "JWT TOKEN";
+
+        // if created, return 201 and user details with token
+        res.status(201).json({
+            userDetails: {
+                mail: user.mail,
+                token: token,
+                username: user.username,
+            }
+        })
+
+    } catch (err) {
+        // if error return 500 and ask user to try again
+        return res.status(500).send("Error occurred. Please try again.");
+    }
 };
 
 // exporting postRegister
