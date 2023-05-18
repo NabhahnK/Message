44ba6c65b64bda8ec327a207292f2f6a23ef5c0d
Nabhahn Kamaal <NabhahnK@gmail.com>
2023-05-18 11:06:25 -0700

    added user, jwt, and bcrypt to postLogin

--- server/controllers/auth/postLogin.js
+++ server/controllers/auth/postLogin.js
@@ -1,3 +1,9 @@
+// import user model
+const User = require('../../models/user');
+// import bcrypt and jwt
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 // declaring postLogin logic
 const postLogin = async (req, res) => {
     res.send('login route');
