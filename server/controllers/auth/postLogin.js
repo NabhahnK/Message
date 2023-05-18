6072972d12ccba4b956b214afec8abcf2fdc0a37
Nabhahn Kamaal <NabhahnK@gmail.com>
2023-05-18 13:39:30 -0700

    added comments to postLogin

--- server/controllers/auth/postLogin.js
+++ server/controllers/auth/postLogin.js
@@ -7,14 +7,18 @@ const jwt = require('jsonwebtoken');
 // declaring postLogin logic
 const postLogin = async (req, res) => {
     try {
+        // get mail from input
         const { mail, password } = req.body;
 
+        // check if user email is in DB
         const user = await User.findOne({ mail: mail.toLowerCase() });
 
+        // check if pass word and email mathc DB
         if (user && (await bcrypt.compare(password, user.password))) {
             // send new token
             const token = 'JWT_TOKEN';
 
+            // return status and user data
             return res.status(200).json({
                 userDetails: {
                     mail: user.mail,
@@ -24,8 +28,10 @@ const postLogin = async (req, res) => {
             })
         }
 
+        // tell user to try again
         return res.status(400).send('Invalid credentials. Please try again.');
     } catch (err) {
+        // server error
         return res.status(500).send('Something went wrong. Please try again.');
     }
 };
